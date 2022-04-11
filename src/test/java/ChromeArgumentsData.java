81c2135d349216be138cccdb1400f048e64698a4
bvvtwix <46612713+bvvtwix@users.noreply.github.com>
2022-04-11 21:39:09 +0500

    delete unused methed

--- src/test/java/ChromeArgumentsData.java
+++ src/test/java/ChromeArgumentsData.java
@@ -2,17 +2,12 @@ public enum ChromeArgumentsData {
 
     KIOSK("--kiosk"),
     HEADLESS("headless"),
-    MAXIMAZE("--start-maximized")
-    ;
+    MAXIMAZE("--start-maximized");
 
     private String param;
 
     ChromeArgumentsData(String param) {
         this.param = param;
-    }
-
-    private String getParam(){
-        return param;
     }
 
     @Override
